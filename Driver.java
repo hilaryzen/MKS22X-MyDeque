2dac48f7d929681d56e8df27a4388aed709f020b
Hilary Zen <hzen00@stuy.edu>
2019-03-22 10:42:21 -0400

    Added toString

--- Driver.java
+++ Driver.java
@@ -1,5 +1,6 @@
 public class Driver {
   public static void main(String[] args) {
     MyDeque<String> data = new MyDeque<String>();
+    System.out.println(data.toString());
   }
 }
--- MyDeque.java
+++ MyDeque.java
@@ -3,17 +3,27 @@ public class MyDeque<E> {
   private int size, start, end;
 
   @SuppressWarnings("unchecked")
+  //Creates an array of size 10
   public MyDeque() {
     data = (E[])new Object[10];
     size = 0;
     start = 0;
-    end = 9;
+    end = 0;
   }
 
+  //Creates an array with the given parameter
   public MyDeque(int initialCapacity) {
     data = (E[])new Object[initialCapacity];
     size = 0;
     start = 0;
-    end = data.length - 1;
+    end = 0;
+  }
+
+  public String toString() {
+    String ans = "{";
+    for (int i = 0; i < size; i++) {
+      ans = ans + data[start + i] + " ";
+    }
+    return ans + "}";
   }
 }
