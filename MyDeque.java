a9a42ed3df169f1c7a4f66c8b3e68498cf7b7c83
Hilary Zen <hzen00@stuy.edu>
2019-03-22 10:28:22 -0400

    Wrote constructor

--- MyDeque.java
+++ MyDeque.java
@@ -1,4 +1,12 @@
 public class MyDeque<E> {
   private E[] data;
   private int size, start, end;
+
+  @SuppressWarnings("unchecked")
+  public MyDeque() {
+    data = (E[])new Object[10];
+    size = 0;
+    start = 0;
+    end = 9;
+  }
 }
