ac5ef38daba0dc4285ea96ade801d4fdae6202fe
Hilary Zen <hzen00@stuy.edu>
2019-03-25 10:52:45 -0400

    Added file

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+  /*Evaluate a postfix expression stored in s.
+   *Assume valid postfix notation, separated by spaces.
+  */
+  public static double eval(String s){
+    String[] args = s.split(s);
+    return 0.0;
+  }
+}
