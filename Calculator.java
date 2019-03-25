34ee086517c00331e3b0a1173aa9c0f849fa7c16
Hilary Zen <hzen00@stuy.edu>
2019-03-25 10:59:24 -0400

    Split equation

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,10 @@ public class Calculator {
    *Assume valid postfix notation, separated by spaces.
   */
   public static double eval(String s){
-    String[] args = s.split(s);
+    String[] args = s.split(" ");
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+    }
     return 0.0;
   }
 }
---
+++ DriverC.java
@@ -0,0 +1,5 @@
+public class DriverC {
+  public static void main(String[] args) {
+    Calculator.eval("10 2 +");
+  }
+}
