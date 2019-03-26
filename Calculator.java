a1158278e53b0732a3b0abc887310abe214049de
Hilary Zen <hzen00@stuy.edu>
2019-03-26 10:57:14 -0400

    Wrote add method

--- Calculator.java
+++ Calculator.java
@@ -4,17 +4,25 @@ public class Calculator {
   */
   public static double eval(String s){
     String[] args = s.split(" ");
-    MyDeque<Integer> values = new MyDeque<Integer>();
+    MyDeque<Double> values = new MyDeque<Double>();
 
     for (int i = 0; i < args.length; i++) {
       try {
-        values.addLast(Integer.parseInt(args[i]));
-        System.out.println(values);
+        values.addLast(Double.parseDouble(args[i]));
+        //System.out.println(values);
       } catch (NumberFormatException e) {
 
       }
     }
 
     return 0.0;
+  }
+
+  public static void add() {
+    Double i = values.getLast();
+    values.removeLast();
+    Double j = values.getLast();
+    values.removeLast();
+    values.addLast(i + j);
   }
 }
